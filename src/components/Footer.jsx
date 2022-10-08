c6e41523f7220568a35956b1b2bd99bf246da130
sencer2022 <senceralpdogdu@gmail.com>
2022-10-09 01:33:20 +0300

    completed Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
                 <FaDribbbleSquare size={30} />
             </div>
         </div>
-        <div className='lg:col-span-3 flex justify-between'>
+        <div className='lg:col-span-2 flex justify-between mt-6'>
             <div>
                 <h6 className='font-medium text-gray-400'>Solutions</h6>
                 <ul>
@@ -26,30 +26,30 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h6 className='font-medium text-gray-400'>Solutions</h6>
+                <h6 className='font-medium text-gray-400'>Support</h6>
                 <ul>
-                    <li className='py-2 text-sm'>Analytics</li>
-                    <li className='py-2 text-sm'>Marketing</li>
-                    <li className='py-2 text-sm'>Commerce</li>
-                    <li className='py-2 text-sm'>Insights</li>
+                    <li className='py-2 text-sm'>Pricing</li>
+                    <li className='py-2 text-sm'>Documantion</li>
+                    <li className='py-2 text-sm'>Guides</li>
+                    <li className='py-2 text-sm'>API Status</li>
                 </ul>
             </div>
             <div>
-                <h6 className='font-medium text-gray-400'>Solutions</h6>
+                <h6 className='font-medium text-gray-400'>Company</h6>
                 <ul>
-                    <li className='py-2 text-sm'>Analytics</li>
-                    <li className='py-2 text-sm'>Marketing</li>
-                    <li className='py-2 text-sm'>Commerce</li>
-                    <li className='py-2 text-sm'>Insights</li>
+                    <li className='py-2 text-sm'>About</li>
+                    <li className='py-2 text-sm'>Blog</li>
+                    <li className='py-2 text-sm'>Jobs</li>
+                    <li className='py-2 text-sm'>Press</li>
+                    <li className='py-2 text-sm'>Careers</li>
                 </ul>
             </div>
             <div>
-                <h6 className='font-medium text-gray-400'>Solutions</h6>
+                <h6 className='font-medium text-gray-400'>Legal</h6>
                 <ul>
-                    <li className='py-2 text-sm'>Analytics</li>
-                    <li className='py-2 text-sm'>Marketing</li>
-                    <li className='py-2 text-sm'>Commerce</li>
-                    <li className='py-2 text-sm'>Insights</li>
+                    <li className='py-2 text-sm'>Clain</li>
+                    <li className='py-2 text-sm'>Policy</li>
+                    <li className='py-2 text-sm'>Terms</li>
                 </ul>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { CgMenu, CgClose } from 'react-icons/cg'
 
 const Navbar = () => {
-    const [nav, setNav] = useState(true);
+    const [nav, setNav] = useState(false);
 
     const handleNav = () => {
         setNav(!nav)
@@ -19,9 +19,9 @@ const Navbar = () => {
                 <li className='p-4'>Contact</li>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
-                {!nav ? <CgClose size={20} /> : <CgMenu size={20} />}
+                {nav ? <CgClose size={20} /> : <CgMenu size={20} />}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>FINANCE DATA</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-b-gray-600'>Home</li>
