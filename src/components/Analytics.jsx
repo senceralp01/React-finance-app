3ba29dec4120b16efe74ce6ccfc8b7d84bf34c53
sencer2022 <senceralpdogdu@gmail.com>
2022-10-08 17:42:12 +0300

    added Newsletter component

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,7 +4,7 @@ import analytics from '../assets/analytics.jpg'
 const Analytics = () => {
   return (
     <div className='bg-white w-full py-16 px-4'>
-        <div className='max-w-1240px mx-auto grid md:grid-cols-2'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
             <img className='w-[500px] mx-auto my-4' src={analytics} alt="analytics" />
             <div className='flex flex-col justify-center'>
                 <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
