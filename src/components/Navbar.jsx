90efc61e703c5965f43ddc9049402f7c8c12c415
sencer2022 <senceralpdogdu@gmail.com>
2022-10-10 12:15:32 +0300

    added useMediaQuery hook in order to hide hamburger menu once screen size become bigger then mobile sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { CgMenu, CgClose } from 'react-icons/cg';
 import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -8,6 +9,9 @@ const Navbar = () => {
     const handleNav = () => {
         setNav(!nav)
     };
+
+    const isMobile = useMediaQuery({ query: `(max-width: 768px)` });
+    console.log(isMobile);
 
     const activated = 'bg-[#71e2be] text-white block p-4 rounded-md text-base font-medium';
     const deactivated = 'text-gray-300 hover:bg-gray-800 hover:text-white block p-4 rounded-md text-base font-medium';
@@ -23,9 +27,9 @@ const Navbar = () => {
                 <NavLink to='/contact' className={({ isActive }) => (isActive ? activated : deactivated)}>Contact</NavLink>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <CgClose size={20} className='cursor-pointer' /> : <CgMenu size={20} className='cursor-pointer' />}
+                {nav&&isMobile ? <CgClose size={20} className='cursor-pointer' /> : <CgMenu size={20} className='cursor-pointer' />}
             </div>
-            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={nav&&isMobile ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#71e2be] m-4'>FINANCE DATA</h1>
                 <ul className='uppercase p-4'>
                     <NavLink onClick={handleNav} to='/' className={({ isActive }) => 'border-b border-b-gray-600' + (isActive ? activated : deactivated)} end>Home</NavLink>
